d21cf2e3a0b3bebfecdce360e69b9b976b989fcf
jeffseverson2001 <jeff@jefflizseverson.com>
2021-09-04 19:18:41 -0500

    Deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true 
 });
 
 // routes
